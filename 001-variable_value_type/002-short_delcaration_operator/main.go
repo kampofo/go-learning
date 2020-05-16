cf5ffe89684e65c030f0656df25d81de701b5725
Kwabena Ampofo <kampof1@students.towson.edu>
2020-05-16 09:56:27 -0700

    Completed Ninja Level 1 exercies

--- 001-variable_value_type/002-short_delcaration_operator/main.go
+++ 001-variable_value_type/002-short_delcaration_operator/main.go
@@ -2,14 +2,22 @@ package main
 
 import "fmt"
 
+// var keyword allows for scoping outside of a function body
+// however limit this usage as much as possible in place of
+var y = 100 + 24
+
+var x int // creates x variable and assigns default or '0' value
+
 func main() {
+
+	// Declares a value and ASSIGNS a VALUE
 	x := 42
+	fmt.Printf("%T\n", x)
 	fmt.Println(x)
 
 	x = 99
 	fmt.Println(x)
 
-	y := 100 + 24
 	fmt.Println(y)
 
 	z := "James Bond"
